80bbb700b15010419951ba99184759041c75f3e0
Aaron Martin <sbbeachvball@gmail.com>
2018-06-30 20:50:27 -0700

    Redux add still working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import './index.css';
 import BlendersTourn from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BlendersTourn pools={ 2 } teamsPerPool={ 5 }/>, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BlendersTourn pools={ 2 } teamsPerPool={ 5 }/>
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
+
+// <Provider store={store}>
+// </Provider>,
+
+
+// <BlendersTourn pools={ 2 } teamsPerPool={ 5 }/>,
+// document.getElementById('root')
